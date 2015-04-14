0941b4a287b4b5372d588fdff0117462293b0da0
Sia <siakaramalegos@gmail.com>
2015-04-13 19:19:08 -0500

    Added concat grunt library and edited gruntfile to make skeleton of commands

--- gruntfile.js
+++ gruntfile.js
@@ -1,11 +1,26 @@
 module.exports = function(grunt){
 
+    // Configuration goes here
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json')
+
+        // Concatenate files configuration
+        concat: {
+          // code here
+        }
+
+        // Minify files configuration
     });
 
-    grunt.registerTask('default', function() {
-      grunt.log.writeln("Hello world");
-    } );
+    // Concatenate files
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    // Minify CSS
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    // Minify JS
+    grunt.loadNpmTasks('grunt-contrib-uglify')
+
+    grunt.registerTask('default', ['concat'] );
 
 };
